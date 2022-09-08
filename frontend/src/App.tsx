25b6faf9285c1e928438c4ac73c9fe15e9d40765
Luis Witts <luis.witts@outlook.com>
2022-09-08 18:44:12 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import NotificationButton from "./components/Notification-Button"
+
 function App() {
-  return(
-    <h1> olá mundo </h1>
+  return (
+    <>
+      <h1> olá mundo </h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
+
 
 }
 
